32e7a5cbc9823884ae672860baff1582dd2b76c3
IqbalBintangsyah <iqbalbintangsyah@gmail.com>
2026-01-12 19:25:02 +0900

    add mysql

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 import { GenshinImpact, LanguageEnum } from 'node-hoyolab'
-import 'mysql'
-
-const COOKIE = {
-		ltokenV2: 'v2_CAISDGM5b3FhcTNzM2d1OBokYjIxZDBmMzgtNmIzYi00MjNhLWI4MzUtM2RhMGEwZTM1ZGRjIM2Y-cIGKLmq918w-MORBUILYmJzX292ZXJzZWE.TUxeaAAAAAAB.MEUCIQDDyzYFYgYgPSuCRdOT9NmERdB3yzrd2eQIHNMTFICHpQIge4EjoE2LnEdI0a4Qot8fwWY6plMefJZ1_2kf99Z-FXI',
-		ltuidV2: parseInt('10772984')
-    };
-
-const UID = 800244745
+import * as mysql from 'mysql'
+import config from './config.js'
 
 async function main() {
+	let con = mysql.createConnection({
+		host: config.HOST_SQL,
+		user: config.USER_SQL,
+		password: config.PASSWORD_SQL
+	});
+	con.connect(function(err) {
+		if (err) throw err;
+		console.log("Connected!");
+	});
+
 	const genshin = new GenshinImpact({
-		cookie: COOKIE,
+		cookie: config.COOKIE,
 		lang: LanguageEnum.ENGLISH,
-		uid: UID
-	})
+		uid: config.UID
+	});
 	// Claim the daily rewards.
-	const claim = await genshin.daily.claim()
-	console.log(claim)
+	const claim = await genshin.daily.claim();
+	console.log(claim);
 
 	// Retrieves daily information.
-	const dailyInfo = await genshin.daily.info()
-	console.log(dailyInfo)
+	const dailyInfo = await genshin.daily.info();
+	console.log(dailyInfo);
 
 	// Retrieve daily rewards information.
-	const rewards = await genshin.daily.rewards()
-	console.log(rewards)
+	const rewards = await genshin.daily.rewards();
+	console.log(rewards);
 
 	// Get the daily reward for a specific day or the current day
-	const reward = await genshin.daily.reward()
-	console.log(reward)
+	const reward = await genshin.daily.reward();
+	console.log(reward);
 }
 
-main()
+main();

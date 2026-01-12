aec442401bc042a667432b18be3ce12d9a81059e
IqbalBintangsyah <iqbalbintangsyah@gmail.com>
2026-01-12 18:58:41 +0900

    add mysql import

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import { GenshinImpact, LanguageEnum } from 'node-hoyolab'
+import 'mysql'
 
 const COOKIE = {
 		ltokenV2: 'v2_CAISDGM5b3FhcTNzM2d1OBokYjIxZDBmMzgtNmIzYi00MjNhLWI4MzUtM2RhMGEwZTM1ZGRjIM2Y-cIGKLmq918w-MORBUILYmJzX292ZXJzZWE.TUxeaAAAAAAB.MEUCIQDDyzYFYgYgPSuCRdOT9NmERdB3yzrd2eQIHNMTFICHpQIge4EjoE2LnEdI0a4Qot8fwWY6plMefJZ1_2kf99Z-FXI',
 		ltuidV2: parseInt('10772984')
     };
 
+const UID = 800244745
+
 async function main() {
 	const genshin = new GenshinImpact({
 		cookie: COOKIE,
 		lang: LanguageEnum.ENGLISH,
+		uid: UID
 	})
 	// Claim the daily rewards.
 	const claim = await genshin.daily.claim()
